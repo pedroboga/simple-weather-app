e92bbda2dba2d71c26abe684dcfb712d0fb2a1a4
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-08-21 11:15:54 -0300

    First part of the UI

--- SimpleWeatherApp/SimpleWeatherApp/ContentView.swift
+++ SimpleWeatherApp/SimpleWeatherApp/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .top,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Curitiba, BR")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("20°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                Spacer()
+            }
+        }
     }
 }
 
