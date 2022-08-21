29ca8665ab27fd98ab097c08d34161d07e89d271
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-08-21 10:48:18 -0300

    Created the project structure

---
+++ SimpleWeatherApp/SimpleWeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleWeatherApp
+//
+//  Created by Pedro Boga on 21/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleWeatherApp/SimpleWeatherApp/SimpleWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherAppApp.swift
+//  SimpleWeatherApp
+//
+//  Created by Pedro Boga on 21/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
